9f96ebaff922f8ffa96c39152f76ac328c249611
Arvin Buid <arvinbuid01@gmail.com>
2024-05-02 16:10:21 +0800

    Adding Navbar component structure and design and importing navbar inside App.tsx. Installed react-icons and use it inside navbar.tsx as navbar links icon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Navbar} from "./components/navbar";
 
 function App() {
   return (
-    <div className='h-dvh w-screen bg-slate-900'>
+    <div className='h-screen w-screen'>
       <Router>
-        <Routes>
-          <Route
-            path='/'
-            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Home</h1>}
-          ></Route>
-          <Route
-            path='/auth'
-            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Auth</h1>}
-          ></Route>
-          <Route
-            path='/rated'
-            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Rated</h1>}
-          ></Route>
-        </Routes>
+        <Navbar />
+        <div className='pt-[80px]'>
+          <Routes>
+            <Route
+              path='/'
+              element={<h1 className='text-center text-slate-800 text-4xl font-bold'>Home</h1>}
+            ></Route>
+            <Route
+              path='/rated'
+              element={<h1 className='text-center text-slate-800 text-4xl font-bold'>Rated</h1>}
+            ></Route>
+            <Route
+              path='/auth'
+              element={<h1 className='text-center text-slate-800 text-4xl font-bold'>Auth</h1>}
+            ></Route>
+          </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,41 @@
+import {Link} from "react-router-dom";
+import {FaHome} from "react-icons/fa";
+import {FaStar} from "react-icons/fa";
+import {CiLogin} from "react-icons/ci";
+
+export const Navbar = () => {
+  return (
+    <nav className='fixed top-0 left-0 right-0 h-[54px] bg-slate-900 border-b border-slate-600 shadow-md    shadow-slate-400'>
+      <ul className='flex gap-6 items-center h-full px-8'>
+        {/* left */}
+        <div className='flex flex-1 gap-6'>
+          <li>
+            <Link to={"/"}>
+              <h2 className='text-slate-100 text-xl'>
+                <FaHome />
+              </h2>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/rated"}>
+              <h2 className='text-slate-100 text-xl'>
+                <FaStar />
+              </h2>
+            </Link>
+          </li>
+        </div>
+
+        {/* right */}
+        <div className='flex h-full items-center justify-center'>
+          <li>
+            <Link to={"/auth"}>
+              <h2 className='text-slate-100 text-xl font-bold'>
+                <CiLogin />
+              </h2>
+            </Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+};
