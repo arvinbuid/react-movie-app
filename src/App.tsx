c8c6a91c757e8ff497486a5cf3bc182c31cf1288
Arvin Buid <arvinbuid01@gmail.com>
2024-05-02 14:56:31 +0800

    Added Routes and links inside App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,23 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className='h-screen w-screen bg-slate-900'>
-      <p className='text-center text-4xl font-semibold text-slate-100 pt-5'>React Movie App</p>
+    <div className='h-dvh w-screen bg-slate-900'>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Home</h1>}
+          ></Route>
+          <Route
+            path='/auth'
+            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Auth</h1>}
+          ></Route>
+          <Route
+            path='/rated'
+            element={<h1 className='text-center text-slate-100 text-4xl font-bold py-6'>Rated</h1>}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
