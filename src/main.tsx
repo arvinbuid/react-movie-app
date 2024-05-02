b6c921563b541cf4ec833af28a0a1c9f666678b7
Arvin Buid <arvinbuid01@gmail.com>
2024-05-02 17:54:15 +0800

    Minor changes inside main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
