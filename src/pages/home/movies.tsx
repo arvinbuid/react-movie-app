cb9e376bdfdae650afdb0a5c71b9d7d5c25aecf6
Arvin Buid <arvinbuid01@gmail.com>
2024-05-04 16:34:14 +0800

    Created Rate UI inside src/pages/home/movies.tsx.

--- src/pages/home/movies.tsx
+++ src/pages/home/movies.tsx
@@ -1,4 +1,6 @@
+import {ChangeEvent, useState} from "react";
 import {Link} from "react-router-dom";
+import {Star} from "lucide-react";
 
 interface DisplayData {
   id: number;
@@ -15,7 +17,18 @@ interface Props {
 }
 
 export const Movies = (props: Props) => {
+  const [rating, setRating] = useState<number>(0);
   const {data} = props;
+
+  const handleRatingChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setRating(Number(e.target.value));
+    console.log();
+  };
+
+  const handleSubmitRating = (e: any) => {
+    e.preventDefault();
+    console.log("Rating: ", rating);
+  };
 
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6 mb-8'>
@@ -40,6 +53,29 @@ export const Movies = (props: Props) => {
               Rating: {displayData.vote_average.toFixed(2)}
             </p>
             <p className='text-mds text-start'>{displayData.overview}</p>
+          </div>
+
+          {/* Rate Section */}
+          <div className='flex flex-col px-8 pb-4'>
+            <div className='flex items-center h-[35px]'>
+              <input
+                type='number'
+                min='0'
+                max='10'
+                step='0.5'
+                className='w-[60px] h-[34px] text-slate-700 text-md font-bold text-center outline-none'
+                onChange={handleRatingChange}
+              />
+              <div className='w-[82px] bg-blue-950 h-full flex items-center justify-center cursor-pointer '>
+                <button
+                  onClick={handleSubmitRating}
+                  className='flex items-center justify-center h-full px-2'
+                >
+                  <p className='text-sm font-semibold'>Rate</p>
+                  <Star className='text-blue-300 h-[14px]'></Star>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       ))}
