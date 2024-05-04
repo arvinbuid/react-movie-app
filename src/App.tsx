4c6b14dfbc9919278ffbe660c42739dcac7ac90b
Arvin Buid <arvinbuid01@gmail.com>
2024-05-04 23:19:12 +0800

    Created Rated component in src/pages/rated/index.tsx. Rated is imported into App.tsx root component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Auth} from "./pages/auth";
 import {Home} from "./pages/home";
 import {Movie} from "./pages/movie";
 import {TvShow} from "./pages/tvshow";
+import {Rated} from "./pages/rated";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route
               path='/rated'
-              element={<h1 className='text-center text-slate-800 text-4xl font-bold'>Rated</h1>}
+              element={
+                <h1 className='text-center text-slate-800 text-4xl font-bold'>{<Rated />}</h1>
+              }
             ></Route>
             <Route path='/auth' element={<Auth />}></Route>
             <Route path='/movie/:id' element={<Movie />}></Route>
