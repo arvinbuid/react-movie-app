82009e98f89240695c5a71416f8b2599503ad799
Arvin Buid <arvinbuid01@gmail.com>
2024-05-06 11:59:49 +0800

    Made some adjustment of styles inside src/pages/rated/index.tsx.

--- src/pages/rated/index.tsx
+++ src/pages/rated/index.tsx
@@ -1,3 +1,27 @@
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+
 export const Rated = () => {
-  return <div></div>;
+  return (
+    <div className='w-screen h-screen flex justify-start mt-6'>
+      <Tabs defaultValue='movies' className='w-[400px]'>
+        <TabsList>
+          <TabsTrigger value='movies'>Movies</TabsTrigger>
+          <TabsTrigger value='tvshows'>TV Shows</TabsTrigger>
+        </TabsList>
+        {/* rated movies */}
+        <TabsContent value='movies'>
+          <div className=' w-screen h-[500px]'>
+            <h1 className='text-4xl text-slate-100 mt-[28px] mb-[6px]'>Rated Movies</h1>
+          </div>
+
+          {/* rated tvshows */}
+        </TabsContent>
+        <TabsContent value='tvshows'>
+          <div className=' w-screen h-[500px]'>
+            <h1 className='text-4xl text-slate-100 mt-[28px] mb-[6px]'>Rated TV Shows</h1>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
